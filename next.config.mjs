c5f5999c8615b1187a8be4b0b73985e89f5d6e99
Prince <167101466+pkprajapati7402@users.noreply.github.com>
2025-04-05 18:42:53 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
         source: "/api",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://toc-pbl.vercel.app" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Origin", value: "https://toc-pbl.vercel.app" }, // Updated to your frontend origin
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
